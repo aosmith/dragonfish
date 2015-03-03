9b2e0131e83e1f93d8dcbcfc43392d93e44a51b1
Alex Smith <aosmith@gmail.com>
2015-03-03 14:28:08 -0600

    fixed handling of relative urls

--- app/jobs/crawl_job.rb
+++ app/jobs/crawl_job.rb
@@ -9,8 +9,8 @@ class CrawlJob < ActiveJob::Base
     TCPSocket::socks_port = 9050
     uri = URI("http://#{args.onion_url}")
     res = Net::HTTP.get(uri).force_encoding(Encoding::UTF_8)
-    relative_urls = res.scan(/<a(.*)href=\\"\/(.*)?\"(^>)?>/)
-    text_only = CGI::unescapeHTML(res.gsub(/<[^>]*>/,""))
+    relative_urls = res.scan(/.*href="(\S*)"/)
+    # text_only = CGI::unescapeHTML(res.gsub(/<[^>]*>/,""))
     onion_urls = res.scan(/(http(s)?\:\/\/)?([A-z0-9]{16}\.onion)(\/)?([^\"](.)*[^\"])?/)
     base_url = args.base_url
     site = Site.find_by({ base_url: base_url })
@@ -19,8 +19,9 @@ class CrawlJob < ActiveJob::Base
     end
     logger.debug res
     relative_urls.each do |u|
+      sanitized = sanitize_relative_url args.base_url, u
       logger.debug "relative url: #{u}"
-      c = CrawlRequest.create({ onion_url: "#{args.base_url}#{relative_url}" })
+      c = CrawlRequest.create({ onion_url: "#{sanitized}" })
       c.save
     end
     onion_urls.each do |o|
@@ -36,6 +37,16 @@ class CrawlJob < ActiveJob::Base
   end
 
   private
+
+  def sanitize_relative_url base, rel
+    if url.start_with? "//"
+      return "http:#{url}"
+    elsif url.start_with? "/"
+      return "http://#{base}#{rel}"
+    elsif url.start_with? "http:"
+      return rel
+    end
+  end
 
   def establish_tor_circuit
     TorControl.password = "TBm8mFTd9mm8qxrRXoARQRHkrfM219WtxSkSejN48Lv"
